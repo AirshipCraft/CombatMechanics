c4e2fd247f4e3a3004a9d3d9105abeba4f52ca50
Locutusque <122424773+Locutusque@users.noreply.github.com>
2023-02-24 17:28:48 -0800

    Update PlayerStats.java

--- src/main/java/tk/airshipcraft/airshipcraftcombat/PlayerStats.java
+++ src/main/java/tk/airshipcraft/airshipcraftcombat/PlayerStats.java
@@ -1,68 +1,103 @@
 package tk.airshipcraft.airshipcraftcombat;
 
-import org.bukkit.entity.Player;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.HashMap;
 
 public class PlayerStats {
-    private double maxHealth;
-    private final double baseDamage;
-    private double health;
-    private double strength;
-    private double critChance;
-    private double critDamage;
-    private double defense;
+    private double defaultHealth;
+    private double defaultStrength;
+    private double defaultCritChance;
+    private double defaultCritDamage;
+    private double defaultDefense;
+    private double defaultBaseDamage;
+    private double defaultMaxHealth;
+
+    private HashMap<LivingEntity, Double> healthMap;
+    private HashMap<LivingEntity, Double> strengthMap;
+    private HashMap<LivingEntity, Double> critChanceMap;
+    private HashMap<LivingEntity, Double> critDamageMap;
+    private HashMap<LivingEntity, Double> defenseMap;
+    private HashMap<LivingEntity, Double> baseDamageMap;
+    private HashMap<LivingEntity, Double> maxHealthMap;
 
     public PlayerStats(double health, double strength, double critChance, double critDamage, double defense, double baseDamage, double maxHealth) {
-        this.health = health;
-        this.strength = strength;
-        this.critChance = critChance;
-        this.critDamage = critDamage;
-        this.defense = defense;
-        this.baseDamage = baseDamage;
-        this.maxHealth = maxHealth;
+        this.healthMap = new HashMap<>();
+        this.strengthMap = new HashMap<>();
+        this.critChanceMap = new HashMap<>();
+        this.critDamageMap = new HashMap<>();
+        this.defenseMap = new HashMap<>();
+        this.baseDamageMap = new HashMap<>();
+        this.maxHealthMap = new HashMap<>();
+
+        this.defaultHealth = health;
+        this.defaultStrength = strength;
+        this.defaultCritChance = critChance;
+        this.defaultCritDamage = critDamage;
+        this.defaultDefense = defense;
+        this.defaultBaseDamage = baseDamage;
+        this.defaultMaxHealth = maxHealth;
     }
 
-    public double getHealth() {
-        return health;
-    }
-    public double getMaxHealth() {
-        return maxHealth;
-    }
-    public double getBaseDamage() {
-        return baseDamage;
-    }
-    public void setHealth(double health) {
-        this.health = health;
+    public double getHealth(LivingEntity playerUUID) {
+        if (healthMap.containsKey(playerUUID)) {
+            return healthMap.get(playerUUID);
+        } else {
+
+            throw new ValueException("Health not found for player " + playerUUID.getName());
+        }
     }
 
-    public double getStrength() {
-        return strength;
+    public double getMaxHealth(LivingEntity entity) {
+        return maxHealthMap.getOrDefault(entity, defaultMaxHealth);
     }
 
-    public void setStrength(double strength) {
-        this.strength = strength;
-    }
-    public double getCritChance() {
-        return critChance;
+    public double getBaseDamage(LivingEntity entity) {
+        return baseDamageMap.getOrDefault(entity, defaultBaseDamage);
     }
 
-    public void setCritChance(double critChance) {
-        this.critChance = critChance;
+    public void setCustomHealth(double health, LivingEntity entity) {
+        healthMap.put(entity, health);
     }
 
-    public double getCritDamage() {
-        return critDamage;
+    public void setCustomStrength(double strength, LivingEntity entity) {
+        strengthMap.put(entity, strength);
     }
 
-    public void setCritDamage(double critDamage) {
-        this.critDamage = critDamage;
+    public void setCustomCritChance(double critChance, LivingEntity entity) {
+        critChanceMap.put(entity, critChance);
     }
 
-    public double getDefense() {
-        return defense;
+    public void setCustomCritDamage(double critDamage, LivingEntity entity) {
+        critDamageMap.put(entity, critDamage);
     }
 
-    public void setDefense(double defense) {
-        this.defense = defense;
+    public void setCustomDefense(double defense, LivingEntity entity) {
+        defenseMap.put(entity, defense);
+    }
+
+    public void setCustomBaseDamage(double baseDamage, LivingEntity entity) {
+        baseDamageMap.put(entity, baseDamage);
+    }
+
+    public void setCustomMaxHealth(double maxHealth, LivingEntity entity) {
+        maxHealthMap.put(entity, maxHealth);
+    }
+
+    public double getStrength(LivingEntity entity) {
+        return strengthMap.getOrDefault(entity, defaultStrength);
+    }
+
+    public double getCritChance(LivingEntity entity) {
+        return critChanceMap.getOrDefault(entity, defaultCritChance);
+    }
+
+    public double getCritDamage(LivingEntity entity) {
+        return critDamageMap.getOrDefault(entity, defaultCritDamage);
+    }
+
+    public double getDefense(LivingEntity entity) {
+        return defenseMap.getOrDefault(entity, defaultDefense);
     }
 }
-
