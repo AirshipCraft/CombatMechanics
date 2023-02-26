1279f712bad241b8ec4884d06809f223db26d047
Locutusque <122424773+Locutusque@users.noreply.github.com>
2023-02-26 11:49:41 -0800

    Update Main.java

--- src/main/java/tk/airshipcraft/airshipcraftcombat/Main.java
+++ src/main/java/tk/airshipcraft/airshipcraftcombat/Main.java
@@ -1,8 +1,10 @@
 package tk.airshipcraft.airshipcraftcombat;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
-
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 public final class Main extends JavaPlugin {
 
     @Override
@@ -11,10 +13,15 @@ public final class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new DamageListener(), this);
     }
 
-
-
     @Override
     public void onDisable() {
-
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                if (!(entity instanceof Player)) {
+                    entity.remove();
+                    // Used to prevent outdated entities
+                }
+            }
+        }
     }
 }
